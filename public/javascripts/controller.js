a26e132050921d79ea65d08c7a38e1cea1aef735
root <root@localhost.localdomain>
2017-11-09 21:43:41 -0600

    update to new DB

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -23,6 +23,16 @@ app.config(function($routeProvider) {
 app.controller('mainController', function($scope,$mdDialog,$http) {
     $scope.username;
     $scope.password;
+    $scope.credit = 0;
+
+    $scope.$vm = {
+        user:{
+            firstname: "",
+            lastname: "",
+            username: "",
+            password: ""
+        }
+    };
 
     $scope.loginCheck = function(){
         $http.post("http://localhost:3000/loginCheck", {alias:$scope.username,password:$scope.password}, {headers: {'Content-Type': 'application/json'} })
@@ -30,12 +40,55 @@ app.controller('mainController', function($scope,$mdDialog,$http) {
             if (response.data == ""){
                 $scope.error = "Usuario o contraseña incorrecta"; 
             }else{
-                location.href ="#menu";
+                location.href ="#menu";_
             }
+        }, function (error) {
+            console.log("error");
         });
     }
-    /*$http.get('http://localhost:3000/test', {}).
-        then(function(response){console.log(response)}, function(error){console.log(error)});*/
+
+    $scope.register = function(){
+        $http.post("http://localhost:3000/insertUser", 
+            {name:$scope.vm.user.firstname,
+            lastname:$scope.vm.user.lastname,
+            alias:$scope.vm.user.username,
+            password:$scope.vm.user.password
+            },
+            {headers: {'Content-Type': 'application/json'} })
+        .then(function (response) {
+            if (response.data == ""){
+                $scope.error = "transacción incorrecta"; 
+            }else{
+                location.href ="#menu";
+            }
+        }, function (error) {
+            console.log("error");
+        });
+    }
+    
+    $scope.getCredit = function(){
+        $http.get('http://localhost:3000/test', {}).
+            then(function(response){
+                console.log(response);
+                $scope.credit = response.data[0].mount;
+            }, function(error){
+                console.log(error)
+            });
+    }
+
+    $scope.newPass = function(){
+        $http.post("http://localhost:3000/PassCheck", {alias:$scope.username}, {headers: {'Content-Type': 'application/json'} })
+        .then(function (response) {
+            if (response.data == ""){
+                $scope.error = "Falló la transacción"; 
+            }else{
+                location.href ="#menu";
+            }
+        }, function (error) {
+            console.log("error");
+        });
+    }
+
     /*$http.post("http://localhost:3000/insertCredit", {mount:500,typePayment:1}, {headers: {'Content-Type': 'application/json'} })
         .then(function (response) {
             return response;
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,30 @@ var router = express.Router();
 
 //Database Conection Mysql
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
+/*//Remoto
+  var connection = mysql.createConnection({
+  host : "85.10.205.173",
+  port : 3307,
+  user     : 'jorge151095',
+  password : '335878531',
+  database : 'bussystem'
+});
+*/
+  var connection = mysql.createConnection({
+  host : "sql9.freesqldatabase.com",
+  port : 3306,
+  user     : 'sql9204108',
+  password : 'mjTXcUADg5',
+  database : 'sql9204108'
+});
+//Local
+/*var connection = mysql.createConnection({
+  host : "localhost",
   user     : 'root',
   password : '',
   database : 'BusSystem'
-});
+});*/
+console.log(connection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -26,7 +44,6 @@ router.post('/insertCredit', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   console.log(req.body.mount);
   console.log(req.body.typePayment);
-
   res.send("");
   //connection.connect();
   /*var sQuery = "INSERT INTO `credit`(`mount`, `type_payment`) VALUES (" + mount + ",'" + typePayment + "')";
@@ -36,6 +53,15 @@ router.post('/insertCredit', function (req, res) {
   });
   //connection.end();
     //res.send('Hello World test!')*/
+})
+
+router.post('/insertUser', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  sQuery = "INSERT INTO `user`(`name`, `last name`, `alias`,`password`) VALUES ('"+req.body.name+"','"+req.body.lastName+"','"+req.body.alias+"','"+req.body.password+"')"; 
+  connection.query(sQuery, function (error, results, fields) {
+    if (error) throw error;
+    res.send(results[0]);
+  });  
 })
 
 router.post('/loginCheck', function (req, res) {
