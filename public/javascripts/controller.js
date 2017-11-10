d35e66be7066b94423036368716954f4d33c23b4
root <root@localhost.localdomain>
2017-11-10 09:23:21 -0600

    finish

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -17,18 +17,30 @@ app.config(function($routeProvider) {
     .when('/menu', {
         templateUrl : '../html/menu.html',
         controller  : 'mainController'
+    })
+    .when('/server', {
+        templateUrl : '../html/server.html',
+        controller  : 'mainController'
     });
 });
 
-app.controller('mainController', function($scope,$mdDialog,$http) {
-    $scope.username;
-    $scope.password;
-    $scope.credit = 0;
+
+app.factory("MyService", function() {
+  return {
+    data: {}
+  };
+});
+
+app.controller('mainController', function($scope,$mdDialog,$http,$location,$timeout,MyService) {
+    //console.log(MyService.data.username);
+    $scope.username = MyService.data.username;
+    $scope.password = "";
+    $scope.credit = MyService.data.credit;
 
     $scope.$vm = {
         user:{
-            firstname: "",
-            lastname: "",
+            firstName: "",
+            lastName: "",
             username: "",
             password: ""
         }
@@ -40,7 +52,13 @@ app.controller('mainController', function($scope,$mdDialog,$http) {
             if (response.data == ""){
                 $scope.error = "Usuario o contraseña incorrecta"; 
             }else{
-                location.href ="#menu";_
+                $timeout(function () {
+                    MyService.data.username = $scope.username;
+                    $scope.$apply(function () {
+                        $scope.getCredit();
+                        $location.path('/menu').replace();
+                   });
+                }, 0);
             }
         }, function (error) {
             console.log("error");
@@ -48,18 +66,24 @@ app.controller('mainController', function($scope,$mdDialog,$http) {
     }
 
     $scope.register = function(){
+        console.log($scope.$vm.user.firstName);
         $http.post("http://localhost:3000/insertUser", 
-            {name:$scope.vm.user.firstname,
-            lastname:$scope.vm.user.lastname,
-            alias:$scope.vm.user.username,
-            password:$scope.vm.user.password
+            {name:$scope.$vm.user.firstName,
+            lastName:$scope.$vm.user.lastName,
+            alias:$scope.$vm.user.username,
+            password:$scope.$vm.user.password
             },
             {headers: {'Content-Type': 'application/json'} })
         .then(function (response) {
             if (response.data == ""){
                 $scope.error = "transacción incorrecta"; 
             }else{
-                location.href ="#menu";
+                $timeout(function () {
+                    $scope.$apply(function () {
+                        $scope.getCredit();
+                        $location.path('/login').replace();
+                   });
+                }, 0);
             }
         }, function (error) {
             console.log("error");
@@ -67,28 +91,49 @@ app.controller('mainController', function($scope,$mdDialog,$http) {
     }
     
     $scope.getCredit = function(){
-        $http.get('http://localhost:3000/test', {}).
+        $http.post('http://localhost:3000/getCredit', {alias:$scope.username}).
             then(function(response){
                 console.log(response);
-                $scope.credit = response.data[0].mount;
+                $scope.credit = response.data.mount;
+                MyService.data.credit = $scope.credit;
             }, function(error){
                 console.log(error)
             });
     }
 
     $scope.newPass = function(){
-        $http.post("http://localhost:3000/PassCheck", {alias:$scope.username}, {headers: {'Content-Type': 'application/json'} })
+        $http.post("http://localhost:3000/newPass", {alias:$scope.username}, {headers: {'Content-Type': 'application/json'} })
         .then(function (response) {
             if (response.data == ""){
                 $scope.error = "Falló la transacción"; 
             }else{
-                location.href ="#menu";
+                alert("transacción correcta");
+                $scope.getCredit();
             }
         }, function (error) {
+            alert("transacción incorrecta");
             console.log("error");
         });
     }
 
+    $scope.findCredit = function(){
+        $http.post("http://localhost:3000/findCredit", {alias:$scope.id}, {headers: {'Content-Type': 'application/json'} })
+        .then(function (response) {
+            if (response.data == 0){
+                alert("Saldo insuficiente");
+            }else{
+                alert("Pasar");
+            }
+        }, function (error) {
+            alert("transacción incorrecta");
+            console.log("error");
+        });
+    }
+
+    $scope.find = function(){
+        console.log("entro");
+        alert($scope.id);
+    }
     /*$http.post("http://localhost:3000/insertCredit", {mount:500,typePayment:1}, {headers: {'Content-Type': 'application/json'} })
         .then(function (response) {
             return response;
--- routes/index.js
+++ routes/index.js
@@ -33,9 +33,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/getCredit', function (req, res) {
-  connection.query('SELECT * FROM credit', function (error, results, fields) {
-    if (error) throw error;
+router.post('/getCredit', function (req, res) {
+  sQuery = "SELECT * FROM `count` INNER JOIN credit ON count.creditId = credit.id INNER JOIN user ON user.id = count.userId WHERE alias = '"+req.body.alias+"'";
+  connection.query(sQuery, function (error, results, fields) {
+    console.log("getCredit:");
+    console.log(results[0]);
+    if (error) {res.send(results)};
     res.send(results[0]);
   });
 })
@@ -58,19 +61,71 @@ router.post('/insertCredit', function (req, res) {
 router.post('/insertUser', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   sQuery = "INSERT INTO `user`(`name`, `last name`, `alias`,`password`) VALUES ('"+req.body.name+"','"+req.body.lastName+"','"+req.body.alias+"','"+req.body.password+"')"; 
+  console.log(sQuery);
   connection.query(sQuery, function (error, results, fields) {
-    if (error) throw error;
-    res.send(results[0]);
+    if (error) {
+      res.send(results)
+    }else{
+      connection.query("INSERT INTO `credit`(`mount`, `type_payment`) VALUES (500,1)", function (error, results, fields) {
+        if (error) {res.send(results)
+        }else{
+          connection.query("SELECT * FROM `credit` ORDER BY id DESC ", function (error, results, fields) {
+            var idcredit = results[0].id;
+            console.log(idcredit);
+            connection.query("SELECT * FROM `user` ORDER BY id DESC ", function (error, results, fields) {
+              var iduser = results[0].id;
+              console.log(iduser);
+              connection.query("INSERT INTO `count`(`creditId`, `state`, `creationDate`, `userId`) VALUES ("+idcredit+",1,0,"+iduser+")", function (error, results, fields) {
+                if(error){res.send(error)}else{res.send(JSON.stringify("OK"));}
+              });
+            });
+          });
+        }
+      });
+    }
   });  
 })
 
 router.post('/loginCheck', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   sQuery = "SELECT * FROM user WHERE alias = '" + req.body.alias + "' AND password = '" + req.body.password + "';" 
+  console.log(sQuery);
   connection.query(sQuery, function (error, results, fields) {
-    if (error) throw error;
-    res.send(results[0]);
+    if (error){
+      res.send(error);
+    }else{
+      console.log(results[0]);
+      res.send(results[0]);
+    }
   });  
+})
+
+router.post('/newPass', function (req, res) {
+  sQuery = "UPDATE credit INNER JOIN `count` ON count.creditId = credit.id INNER JOIN user ON user.id = count.userId SET mount = mount -10 WHERE alias = '"+req.body.alias+"'";
+  console.log(sQuery);
+  connection.query(sQuery, function (error, results, fields) {
+    if (error) {
+      res.send(error);
+    }else{
+      res.send(JSON.stringify("OK"));
+    }
+  });
+})
+
+router.post('/findCredit', function (req, res) {
+  sQuery = "SELECT COUNT(user.id) AS count FROM `count` INNER JOIN credit ON count.creditId = credit.id INNER JOIN user ON user.id = count.userId WHERE user.id = '"+req.body.alias+"' AND MOUNT > -10";
+  console.log(sQuery);
+  connection.query(sQuery, function (error, results, fields) {
+    if (error) {
+      res.send(error);
+    }else{
+      if(results[0].count == 1){
+      res.send(JSON.stringify(1));}
+      else{
+        res.send(JSON.stringify(0));
+      }
+    }
+  });
 })
 
 module.exports = router;
